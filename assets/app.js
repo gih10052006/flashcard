3ae54f66104921ec51e34d79369c50da086cba91
gih10052006 <163078734+gih10052006@users.noreply.github.com>
2024-10-09 22:08:18 -0300

    09-10-2024

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+<script src="app.js"></script>
+<script src="perguntas.js"></script>
+function criaCartao (categoria, pergunta, resposta) {
+
+}
+function criaCartao (categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
